f6049313b14f03765ff05dd044b69bce00674328
Hirotaka Nakajima <hiro@skyblue.me.uk>
2012-10-11 10:44:20 -0400

    whois 5.20
    Closes #100.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ whois.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Whois < Formula
+  homepage 'http://packages.debian.org/sid/whois'
+  url 'http://ftp.debian.org/debian/pool/main/w/whois/whois_5.0.20.tar.xz'
+  sha1 '0fe1c6b4cc8434bb7f4af1a51bae82868208c50b'
+
+  depends_on 'xz' => :build
+
+  def install
+    system "make whois"
+    bin.install "whois"
+    man1.install "whois.1"
+  end
+
+  def caveats; <<-EOS.undent
+    Debian whois has been installed as `whois` and may shadow the
+    system binary of the same name.
+    EOS
+  end
+
+  def test
+    system "#{bin}/whois"
+  end
+end
