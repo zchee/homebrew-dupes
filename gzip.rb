5ed0acec0d5d65bb3579b5e94e29b32fa91cbcf9
Trevor Wennblom <trevor@well.com>
2012-11-15 10:55:07 -0600

    New formula: gzip 1.5
    Closes #118.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ gzip.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Gzip < Formula
+  homepage 'http://www.gnu.org/software/gzip'
+  url 'http://ftpmirror.gnu.org/gzip/gzip-1.5.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/gzip/gzip-1.5.tar.gz'
+  sha1 '56a80da7b032107372d3f3343bed7c7af452a826'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "#{bin}/gzip", "-V"
+  end
+end
