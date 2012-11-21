4fc048cdfa7895350d575faf7f8fee2337746906
Turadg Aleahmad <turadg@aleahmad.net>
2012-11-21 10:30:12 -0500

    unzip 6.0
    OS X <=10.8 have unzip < 6, which don't support ZIP64 files

---
+++ unzip.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Unzip < Formula
+  homepage 'http://www.info-zip.org/pub/infozip/UnZip.html'
+  url 'http://sourceforge.net/projects/infozip/files/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz'
+  version '6.0'
+  sha1 'abf7de8a4018a983590ed6f5cbd990d4740f8a22'
+
+  keg_only :provided_by_osx
+
+  def install
+    system "make -f unix/Makefile macosx"
+    system "make prefix=#{prefix} MANDIR=#{man} install"
+  end
+
+  def test
+    system "#{bin}/unzip", "--help"
+  end
+end
