57883fd8a2061923741f490302a02b6d57565bc4
Trevor Wennblom <t@trevorw.tw>
2012-11-26 17:49:31 -0600

    openssh v6.1p1
    Closes #123.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- openssh.rb
+++ openssh.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Openssh < Formula
   homepage 'http://www.openssh.com/'
-  url 'http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.0p1.tar.gz'
-  version '6.0p1'
-  sha1 'f691e53ef83417031a2854b8b1b661c9c08e4422'
+  url 'http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.1p1.tar.gz'
+  version '6.1p1'
+  sha1 '751c92c912310c3aa9cadc113e14458f843fc7b3'
 
   def install
     system "./configure", "--with-libedit", "--prefix=#{prefix}"
