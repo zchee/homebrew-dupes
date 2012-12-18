11ee3f044c0fa20495d006fa30bce7541fc97f98
Richard Petri <dasricht@gmail.com>
2012-12-18 21:42:29 +0100

    bison 2.7
    Closes #133.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- bison.rb
+++ bison.rb
@@ -2,11 +2,9 @@ require 'formula'
 
 class Bison < Formula
   homepage 'http://www.gnu.org/software/bison/'
-  url 'http://ftpmirror.gnu.org/bison/bison-2.6.2.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/bison/bison-2.6.2.tar.xz'
-  sha1 'eb0ddc583954d435d95ec6c42ba168dedc09b9b7'
-
-  depends_on 'xz' => :build
+  url 'http://ftpmirror.gnu.org/bison/bison-2.7.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/bison/bison-2.7.tar.gz'
+  sha1 'aa4f5aa51ee448bac132041df0ce25a800a3661c'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
