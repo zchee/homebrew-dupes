21b26838fe2f2238844e3818a222efcc224b4496
Derek Thomas <derekathomas@gmail.com>
2012-12-18 10:02:30 +0900

    Specify TCL_LIBRARY as the linked library in HOMEBREW_PREFIX
    This fixes a problem with tcl-tk in which tk begins looking for a script
    file inside the TCL library.
    
    For example, in python with `import Tkinter ; root = Tkinter.Tk()`, the
    following error occurs:
    
        Traceback (most recent call last):
        File "<stdin>", line 1, in <module>
        File "/usr/local/Cellar/python/2.7.3/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk/Tkinter.py", line 1685, in __init__
            self.tk = _tkinter.create(screenName, baseName, className, interactive, wantobjects, useTk, sync, use)
        _tkinter.TclError: Can't find a usable tk.tcl in the following directories:
            /usr/local/Cellar/tcl/8.5.9/lib/tcl8.5/tk8.5
            /usr/local/Cellar/tcl/8.5.9/lib/tcl8.5/tk8.5/Resources/Scripts
            /usr/local/Cellar/tcl/8.5.9/lib/tk8.5
            /usr/local/Cellar/tcl/8.5.9/lib/tk8.5/Resources/Scripts
            /usr/local/Cellar/python/2.7.3/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/lib/tk8.5
            /usr/local/Cellar/python/2.7.3/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/lib/tk8.5/Resources/Scripts
            /usr/local/Cellar/python/2.7.3/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/lib/tk8.5
            /usr/local/Cellar/python/2.7.3/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/library
    
        This probably means that tk wasn't installed properly.
    
    Closes #132.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- tcl.rb
+++ tcl.rb
@@ -14,6 +14,14 @@ class Tcl < Formula
     args << "--enable-64bit" if MacOS.prefer_64_bit?
 
     cd 'unix' do
+      # required because the tcl library is in the Cellar far away from other
+      # libraries. Communication with tk will get confused and look for the
+      # tk-scripts inside tcl-prefix. Upstream inquiry:
+      # http://sourceforge.net/mailarchive/forum.php?thread_name=CAEoiczcr%2ByOu8XYy8Erfb%3DxOUb8mriDewKZw7Sse3mtoG9AEJg%40mail.gmail.com&forum_name=tcl-core
+      inreplace 'Makefile.in' do |s|
+        s.change_make_var! "TCL_LIBRARY", "#{HOMEBREW_PREFIX}/lib/tcl$(VERSION)"
+      end
+
       system "./configure", *args
       system "make"
       system "make test"
