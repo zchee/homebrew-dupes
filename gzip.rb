6a3dbf3a1b32cd9d81eaee812c48b351a92f3fe3
Brian Gilbert <brian@realityloop.com>
2013-01-29 11:11:34 +1100

    gzip: add support for --rsyncable option
    Closes #138.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gzip.rb
+++ gzip.rb
@@ -6,6 +6,11 @@ class Gzip < Formula
   mirror 'http://ftp.gnu.org/gnu/gzip/gzip-1.5.tar.gz'
   sha1 '56a80da7b032107372d3f3343bed7c7af452a826'
 
+  # Add support for --rsyncable option
+  def patches
+    "http://cvs.pld-linux.org/cgi-bin/viewvc.cgi/cvs/packages/gzip/gzip-rsyncable.patch?revision=1.4&view=co"
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
