e05301f44ea570bc8f443fd155060610a2c26ad4
Jason Yosinski <jason@yosinski.com>
2013-02-05 13:51:25 -0500

    lsof 4.87
    Closes #147.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- lsof.rb
+++ lsof.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Lsof < Formula
   homepage 'http://people.freebsd.org/~abe/'
-  url 'ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.86.tar.bz2'
-  sha1 '53c124fe6fddbc96f4724abec16572d9239f913e'
+  url 'ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.87.tar.bz2'
+  sha1 '68e61e0a45491420ace80770157206328c27d8c4'
 
   def install
-    system "tar xf lsof_4.86_src.tar"
-    cd "lsof_4.86_src" do
+    system "tar xf lsof_4.87_src.tar"
+    cd "lsof_4.87_src" do
       mv "00README", "../README"
       system "./Configure -n darwin"
       system "make"
