9b9d7d294a0dae53f649ffc2db21fa4e296633f5
Jack Nagel <jacknagel@gmail.com>
2013-02-09 16:36:07 -0600

    groff 1.22.2

--- groff.rb
+++ groff.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Groff < Formula
   homepage 'http://www.gnu.org/software/groff/'
-  url 'http://ftpmirror.gnu.org/groff/groff-1.21.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/groff/groff-1.21.tar.gz'
-  sha1 'a513aca4a7530a6e63325addd6ba2d282c8f1608'
+  url 'http://ftpmirror.gnu.org/groff/groff-1.22.2.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/groff/groff-1.22.2.tar.gz'
+  sha1 '37223941e25bb504bf54631daaabb01b147dc1d3'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--without-x"
