20ec476eafe37a0fd62c924f160eeae0fdf37303
Felix Bünemann <buenemann@louis.info>
2013-02-11 14:02:16 +0100

    tidy: Add tidy-html5 as --HEAD
    This build tidy from the experimental html5 fork which adds support for
    validating html5 syntax.
    
    More info:
    http://w3c.github.com/tidy-html5/
    https://github.com/w3c/tidy-html5/
    
    Closes #150

--- tidy.rb
+++ tidy.rb
@@ -5,8 +5,10 @@ class Tidy < Formula
   url 'ftp://mirror.internode.on.net/pub/gentoo/distfiles/tidy-20090325.tar.bz2'
   sha1 '28c000a2cd40262fc0d7c2c429eb2a09b2df7bf4'
 
-  depends_on :automake
-  depends_on :libtool
+  head 'https://github.com/w3c/tidy-html5.git'
+
+  depends_on :automake => :build
+  depends_on :libtool => :build
 
   def install
     system 'sh', 'build/gnuauto/setup.sh'
