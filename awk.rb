e484ecc9bb76e2fede8d4d93fba14672aa4cf75e
Misty De Meo <mistydemeo@gmail.com>
2013-02-12 09:51:35 -0600

    awk 20121220

--- awk.rb
+++ awk.rb
@@ -3,11 +3,18 @@ require 'formula'
 class Awk < Formula
   homepage 'http://www.cs.princeton.edu/~bwk/btl.mirror/'
   url 'http://www.cs.princeton.edu/~bwk/btl.mirror/awk.tar.gz'
-  version '20110810'
-  sha1 '333d93c746aa16e33c5c4aaf505009914cea4770'
+  version '20121220'
+  sha1 '538fd69fb0fd01966eb41ad20c336215b4a07301'
 
   def install
     ENV.O3 # Docs recommend higher optimization
+    # the yacc command the makefile uses results in build failures:
+    # /usr/bin/bison: missing operand after `awkgram.y'
+    # makefile believes -S to use sprintf instead of sprint, but the
+    # -S flag is not supported by `bison -y`
+    inreplace "makefile" do |s|
+      s.change_make_var! 'YACC', 'yacc -d'
+    end
     system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
     bin.install 'a.out' => 'awk'
     man1.install 'awk.1'
