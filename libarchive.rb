6851a46f18adf67326552f1f21bf82c67dd14f82
waltari <gitting.crew@gmail.com>
2013-02-21 16:47:12 +0900

    libarchive 3.1.2
    Closes #158.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- libarchive.rb
+++ libarchive.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libarchive < Formula
   homepage 'http://www.libarchive.org'
-  url 'http://www.libarchive.org/downloads/libarchive-3.1.1.tar.gz'
-  sha1 'ee92567d68f2d7c2489c9c539834efa8a02106df'
+  url 'http://www.libarchive.org/downloads/libarchive-3.1.2.tar.gz'
+  sha1 '6a991777ecb0f890be931cec4aec856d1a195489'
 
   def install
     system "./configure", "--prefix=#{prefix}"
