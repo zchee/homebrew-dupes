d7c9d98f5280318d9d4bbdb86ea9ca6898654254
ishtu <asymmetric.world@gmail.com>
2013-03-06 09:25:32 +0300

    libpcap: add --HEAD build
    Closes #161.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- libpcap.rb
+++ libpcap.rb
@@ -5,6 +5,8 @@ class Libpcap < Formula
   url 'http://www.tcpdump.org/release/libpcap-1.3.0.tar.gz'
   sha1 '90aa2f0fcfb4bf79fc50ccdaa70f484d96295be3'
 
+  head 'git://bpf.tcpdump.org/libpcap'
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
