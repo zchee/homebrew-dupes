61ea83e1568061e04582470089e174cc4c0315d0
Johan Carlquist <carlquist@gmail.com>
2013-03-13 20:53:32 +0100

    heimdal: x11 is a recommended dep
    Closes #165.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- heimdal.rb
+++ heimdal.rb
@@ -5,9 +5,18 @@ class Heimdal < Formula
   url 'http://h5l.org/dist/src/heimdal-1.5.2.tar.gz'
   sha1 'dd0920a181d18236432e4b3e5eab6e468cda4b89'
 
+  depends_on :x11 => :recommended
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make install"
+      args = %W[
+          --disable-debug
+          --disable-dependency-tracking
+          --prefix=#{prefix}
+      ]
+
+      args << "--without-x" unless build.with? "x11"
+
+      system "./configure", *args
+      system "make install"
   end
 end
