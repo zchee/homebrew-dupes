669d2dd542d91b839c49960ddb80d64b08f01255
Jack Nagel <jacknagel@gmail.com>
2013-03-20 19:00:46 -0500

    perl 5.14.4

--- perl.rb
+++ perl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Perl < Formula
   homepage 'http://www.perl.org/'
-  url 'http://www.cpan.org/src/5.0/perl-5.14.3.tar.gz'
-  sha1 'c9bed7b07ab9df485e8fd6c86267e608cbcb4ad2'
+  url 'http://www.cpan.org/src/5.0/perl-5.14.4.tar.gz'
+  sha1 '2fdbbf39b3bbd79e42a7cf42fc05bf9891f077a1'
 
   option 'use-threads', 'Enable Perl threads'
 
