61933bc0260da53a83804485a877c2dbb8ef8798
Keith Watson <ikawnoclast@gmail.com>
2013-03-22 07:02:55 -0400

    openssh: 6.2p1
    Closes #167

--- openssh.rb
+++ openssh.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Openssh < Formula
   homepage 'http://www.openssh.com/'
-  url 'http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.1p1.tar.gz'
-  version '6.1p1'
-  sha1 '751c92c912310c3aa9cadc113e14458f843fc7b3'
+  url 'http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.2p1.tar.gz'
+  version '6.2p1'
+  sha1 '8824708c617cc781b2bb29fa20bd905fd3d2a43d'
 
   option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
 
