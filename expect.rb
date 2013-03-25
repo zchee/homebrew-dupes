9b3bbfa5d4b95de84fb19a82278aefc66a492d54
Samuel John <github@SamuelJohn.de>
2013-03-25 22:20:02 +0100

    expect: Optional --with-brewed-tk

--- expect.rb
+++ expect.rb
@@ -6,12 +6,16 @@ class Expect < Formula
   sha1 'e634992cab35b7c6931e1f21fbb8f74d464bd496'
 
   option 'enable-threads', 'Build with multithreading support'
+  option 'with-brewed-tk', "Use Homebrew's Tk (has optional Cocoa and threads support)"
+
+  depends_on 'homebrew/dupes/tcl-tk' if build.with? 'brewed-tk'
 
   def install
     args = ["--prefix=#{prefix}", "--exec-prefix=#{prefix}", "--mandir=#{man}"]
     args << "--enable-shared"
     args << "--enable-threads" if build.include? "enable-threads"
     args << "--enable-64bit" if MacOS.prefer_64_bit?
+    args << "--with-tcl=#{Formula.factory('tcl-tk').opt_prefix}/lib" if build.with? 'brewed-tk'
 
     system "./configure", *args
     system "make"
