981742cedfa8ec5dd686e40472797d4a32e7c3db
Jack Nagel <jacknagel@gmail.com>
2013-03-27 00:14:51 -0500

    diffutils 3.3

--- diffutils.rb
+++ diffutils.rb
@@ -2,9 +2,11 @@ require 'formula'
 
 class Diffutils < Formula
   homepage 'http://www.gnu.org/s/diffutils/'
-  url 'http://ftpmirror.gnu.org/diffutils/diffutils-3.2.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/diffutils/diffutils-3.2.tar.gz'
-  sha1 'e145a54a8ff67fa6272de48ec0e2f4897f05dce7'
+  url 'http://ftpmirror.gnu.org/diffutils/diffutils-3.3.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/diffutils/diffutils-3.3.tar.xz'
+  sha1 '6463cce7d3eb73489996baefd0e4425928ecd61e'
+
+  depends_on 'xz' => :build
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
