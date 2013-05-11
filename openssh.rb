d4cfa61cc4855b63b803e331301bb21c7f12b677
Collin Anderson <collinanderson@flamingpenguin.metacollin.net>
2013-05-11 06:18:10 -0600

    OpenSSH: Keychain support
    * Adds an option '--with-keychain-support' that adds native OS X
      Keychain capabilty and LaunchDaemon capabilities back to ssh-agent.
    * Added caveat.
    
    Closes #187.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- openssh.rb
+++ openssh.rb
@@ -12,7 +12,19 @@ class Openssh < Formula
   depends_on 'ldns' => :optional
   depends_on 'pkg-config' => :build if build.with? "ldns"
 
+  def patches
+    p = []
+    p << 'https://gist.github.com/metacollin/5559308/raw/96adc2d51c722a799de95e2e9f391bba24bcf371/openssh-6.2p1.patch' if build.include? 'with-keychain-support'
+    p
+  end
+
+
   def install
+    if build.include? "with-keychain-support"
+        ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
+        ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
+    end
+
     args = %W[
       --with-libedit
       --with-kerberos5
@@ -27,4 +39,22 @@ class Openssh < Formula
     system "make"
     system "make install"
   end
+
+  def caveats
+    if build.include? "with-keychain-support"
+      <<-EOS.undent
+        For complete functionality, please modify:
+          /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
+
+        and change ProgramArugments from
+          /usr/bin/ssh-agent
+        to
+          /usr/local/bin/ssh-agent
+
+        After that, you can start storing private key passwords in
+        your OS X Keychain.
+      EOS
+    end
+  end
 end
+
