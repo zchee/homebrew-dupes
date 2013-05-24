96f5420faaf19387591c6f99f125adb1329a5116
sisidovski <arerreee@gmail.com>
2013-05-24 14:35:33 +0900

    ant-1.9.1
    ant-1.9.0 is 404
    
    Closes #191.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- ant.rb
+++ ant.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ant < Formula
   homepage 'http://ant.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.0-bin.tar.gz'
-  sha1 '791418e7e80e3f28d6347528e8992a14f09058e7'
+  url 'http://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.1-bin.tar.gz'
+  sha1 '75942a23d7a9511db32c51f935e923d7c0878f7e'
 
   def install
     rm Dir['bin/*.{bat,cmd,dll,exe}']
