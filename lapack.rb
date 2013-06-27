555e6d72c386c6848aeed48516cee90dbecb323c
Jack Nagel <jacknagel@gmail.com>
2013-06-26 21:54:45 -0500

    lapack: use depends_on :fortran

--- lapack.rb
+++ lapack.rb
@@ -5,11 +5,11 @@ class Lapack < Formula
   url 'http://www.netlib.org/lapack/lapack-3.4.2.tgz'
   sha1 '93a6e4e6639aaf00571d53a580ddc415416e868b'
 
+  depends_on :fortran
+
   keg_only :provided_by_osx
 
   def install
-    ENV.fortran
-
     # Copy over make.inc, to load in configuration for this platform
     mv "INSTALL/make.inc.gfortran", "make.inc"
     system "make", "lib"
