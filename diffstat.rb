4887eab84ca3c6b929177e05bc22015d08feb961
Jack Nagel <jacknagel@gmail.com>
2013-06-30 09:52:46 -0500

    diffstat 1.57
    Closes #205.

--- diffstat.rb
+++ diffstat.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Diffstat < Formula
   homepage 'http://invisible-island.net/diffstat/'
   url 'http://invisible-island.net/datafiles/release/diffstat.tar.gz'
-  version '1.56'
-  sha1 '034c3a72e5d06f5433cdba8c1b6d25146dd699d1'
+  version '1.57'
+  sha1 'c6d9247b8b32eeab5d866686f91a8fbdde3e340f'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
