8e6b7a6714496946ca3e908d228a3a57e7572f08
Jack Nagel <jacknagel@gmail.com>
2013-07-16 15:57:23 -0500

    unzip: fix audit warning

--- unzip.rb
+++ unzip.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Unzip < Formula
   homepage 'http://www.info-zip.org/pub/infozip/UnZip.html'
-  url 'http://sourceforge.net/projects/infozip/files/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz'
+  url 'http://downloads.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz'
   version '6.0'
   sha1 'abf7de8a4018a983590ed6f5cbd990d4740f8a22'
 
