e8fd1bf3993828c4e3031d9bb21950203371ad95
Alexis Hildebrandt <afh@surryhill.net>
2013-08-17 09:12:47 +0200

    tcpdump 4.4.0
    Closes #217.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- tcpdump.rb
+++ tcpdump.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tcpdump < Formula
   homepage 'http://www.tcpdump.org/'
-  url 'http://www.tcpdump.org/release/tcpdump-4.3.0.tar.gz'
-  sha1 '5d0432e4831ca81633a6c9da732caad77d64a9ac'
+  url 'http://www.tcpdump.org/release/tcpdump-4.4.0.tar.gz'
+  sha1 '6c1e72ae32eabb3ab823ff366a2d2f48ad4695f4'
 
   head 'git://bpf.tcpdump.org/tcpdump'
 
