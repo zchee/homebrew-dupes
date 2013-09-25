7df873cda8747c3342be3074e9b057f7e880abfc
Michelangelo De Simone <micdesim@gmail.com>
2013-09-25 15:20:47 -0700

    zlib: fix configure script to detect compiler
    The configure script relies on "gcc -v" to determine the compiler in
    use on the system ("gcc" or "clang").
    
    Fact is, on OS X "cc" is a symlink to clang.
    
    Fixes #228.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- zlib.rb
+++ zlib.rb
@@ -9,9 +9,29 @@ class Zlib < Formula
 
   option :universal
 
+  def patches
+    # configure script fails to detect the right compiler when "cc" is the
+    # default compiler. zlib mantainers have been notified of the issue.
+    DATA
+  end
+
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index b77a8a8..54f33f7 100755
+--- a/configure
++++ b/configure
+@@ -159,6 +159,7 @@ case "$cc" in
+ esac
+ case `$cc -v 2>&1` in
+   *gcc*) gcc=1 ;;
++  *clang*) gcc=1 ;;
+ esac
+ 
+ show $cc -c $test.c
