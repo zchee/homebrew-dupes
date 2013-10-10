7b0344f8cab4952d7ef3c7c5fbeeb01773902c97
archstar <archstar@git.fake>
2013-10-10 15:49:16 -0600

    Download rsync using https.
    Closes #235.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- rsync.rb
+++ rsync.rb
@@ -1,17 +1,17 @@
 require 'formula'
 
 class Rsync < Formula
-  homepage 'http://rsync.samba.org/'
-  url 'http://rsync.samba.org/ftp/rsync/src/rsync-3.1.0.tar.gz'
+  homepage 'https://rsync.samba.org/'
+  url 'https://rsync.samba.org/ftp/rsync/src/rsync-3.1.0.tar.gz'
   sha1 'eb58ab04bcb6293da76b83f58327c038b23fcba3'
 
   depends_on :autoconf
 
   def patches
     %W[
-      http://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;f=fileflags.diff;hb=v3.1.0
-      http://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;f=crtimes.diff;hb=v3.1.0
-      http://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;f=hfs-compression.diff;hb=v3.1.0
+      https://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;f=fileflags.diff;hb=v3.1.0
+      https://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;f=crtimes.diff;hb=v3.1.0
+      https://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;f=hfs-compression.diff;hb=v3.1.0
     ]
   end
 
