5bdbb0c5615b7b9c4f34e3b66bf7a56f88694fc3
Matt Sicker <boards@gmail.com>
2013-10-10 20:35:59 -0500

    make 4.0
    Closes #236.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- make.rb
+++ make.rb
@@ -2,16 +2,23 @@ require 'formula'
 
 class Make < Formula
   homepage 'http://www.gnu.org/software/make/'
-  url 'http://ftpmirror.gnu.org/make/make-3.82.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/make/make-3.82.tar.gz'
-  sha1 '92d1b87a30d1c9482e52fb4a68e8a355e7946331'
+  url 'http://ftpmirror.gnu.org/make/make-4.0.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/make/make-4.0.tar.gz'
+  sha1 'b092020919f74d56118eafac2c202f25ff3b6e59'
 
   keg_only :provided_by_osx
 
+  depends_on 'guile' => :optional
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+
+    args << '--with-guile' if build.with? 'guile'
+    system './configure', *args
     system "make install"
   end
 end
