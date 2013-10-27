bcf6f4a6cfe542aa7a6480cedb41ed186cf4bf82
Matt Sicker <boards@gmail.com>
2013-10-27 12:17:30 -0500

    grep 2.15
    Closes #243.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- grep.rb
+++ grep.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Grep < Formula
   homepage 'http://www.gnu.org/software/grep/'
-  url 'http://ftpmirror.gnu.org/grep/grep-2.14.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/grep/grep-2.14.tar.xz'
-  sha256 'e70e801d4fbb16e761654a58ae48bf5020621c95c8e35bd864742577685872e1'
+  url 'http://ftpmirror.gnu.org/grep/grep-2.15.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/grep/grep-2.15.tar.xz'
+  sha256 'bf5a834e587974c8c64e71b35b9e75cd21c7ff253c0e7fbfb0a78be794965314'
 
   depends_on 'xz' => :build
   depends_on 'pcre'
