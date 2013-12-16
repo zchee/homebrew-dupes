16aa1f51cd94e82efcfca5c71075245e9039d058
chdiza <chdiza@gmail.com>
2013-12-15 23:14:14 -0500

    gzip 1.6
    Closes #259.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gzip.rb
+++ gzip.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gzip < Formula
   homepage 'http://www.gnu.org/software/gzip'
-  url 'http://ftpmirror.gnu.org/gzip/gzip-1.5.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gzip/gzip-1.5.tar.gz'
-  sha1 '56a80da7b032107372d3f3343bed7c7af452a826'
+  url 'http://ftpmirror.gnu.org/gzip/gzip-1.6.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/gzip/gzip-1.6.tar.gz'
+  sha1 '7ec6403090b3eaeb53ef017223cb8034eebc1f49'
 
   # Add support for --rsyncable option
   def patches
