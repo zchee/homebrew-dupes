5237f9a4473886921c187ecc025b48fe8486f656
Kenny Root <kenny@the-b.org>
2013-12-24 23:59:42 -0600

    openssh: restore keychain and gsskex patches
    This restores the keychain and gsskex patches.
    
    The ssh-add command showed the "-M" and "-m" options but wouldn't allow
    you to specify them since they weren't added to the getopt(...) call.
    Point the patch URL to a corrected gist.
    
    Closes #237.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- openssh.rb
+++ openssh.rb
@@ -7,15 +7,31 @@ class Openssh < Formula
   sha1 'cf5fe0eb118d7e4f9296fbc5d6884965885fc55d'
 
   option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+  option 'with-keychain-support', 'Add native OS X Keychain and Launch Daemon support to ssh-agent'
+  option 'with-gssapi-support', 'Add support to GSSAPI key exchange'
 
+  depends_on 'autoconf' => :build if build.with? 'keychain-support'
   depends_on 'openssl' if build.with? 'brewed-openssl'
   depends_on 'ldns' => :optional
   depends_on 'pkg-config' => :build if build.with? "ldns"
 
-  # Note that the keychain and GSSAPI patches have been removed as
-  # they no longer apply against 6.4p1. Pull requests welcome!
+  def patches
+    p = []
+    # Apply a revised version of Simon Wilkinson's gsskex patch (http://www.sxw.org.uk/computing/patches/openssh.html), which has also been included in Apple's openssh for a while
+    p << 'https://trac.macports.org/export/112514/trunk/dports/net/openssh/files/0002-Apple-keychain-integration-other-changes.patch' if build.include? 'with-keychain-support'
+    p << 'https://gist.github.com/kruton/8120594/raw/74cab9ac1fa02505547a6afd7cf5e6b2b2428ed8/gistfile1.txt' if build.include? 'with-keychain-support'
+    p << 'https://trac.macports.org/export/112514/trunk/dports/net/openssh/files/openssh-6.3p1-gsskex-all-20130920.patch' if build.with? 'gssapi-support'
+    p
+  end
 
   def install
+    system "autoreconf -i" if build.with? 'keychain-support'
+
+    if build.include? "with-keychain-support"
+        ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
+        ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
+    end
+
     args = %W[
       --with-libedit
       --with-kerberos5
