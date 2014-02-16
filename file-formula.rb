153ff942de3512935cd25b709082374b7f1f5412
Misty De Meo <mistydemeo@gmail.com>
2014-02-16 01:19:15 -0800

    file-formula 5.17

--- file-formula.rb
+++ file-formula.rb
@@ -3,9 +3,9 @@ require 'formula'
 # "File" is a reserved class name
 class FileFormula < Formula
   homepage 'http://www.darwinsys.com/file/'
-  url 'ftp://ftp.astron.com/pub/file/file-5.15.tar.gz'
-  mirror 'http://fossies.org/unix/misc/file-5.15.tar.gz'
-  sha1 'de1a060aa5fe61c1a6f0359fb526e824b4244323'
+  url 'ftp://ftp.astron.com/pub/file/file-5.17.tar.gz'
+  mirror 'http://fossies.org/unix/misc/file-5.17.tar.gz'
+  sha1 'f7e837a0d3e4f40a02ffe7da5e146b967448e0d8'
 
   head 'https://github.com/glensc/file.git'
 
@@ -14,16 +14,7 @@ class FileFormula < Formula
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--bindir=#{libexec}",
                           "--enable-fsect-man5"
     system "make install"
-
-    # one of the regexes in file 5.15 fails when the locale is
-    # set to UTF-8, so force C; reported upstream:
-    # http://bugs.gw.com/view.php?id=292
-    (bin/'file').write <<-EOS.undent
-    #!/bin/bash
-    LC_CTYPE=C exec #{libexec}/file $@
-    EOS
   end
 end
