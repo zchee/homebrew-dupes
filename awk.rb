c1ce23f55c11ff877868eee5d48983d5595e3028
Jack Nagel <jacknagel@gmail.com>
2014-03-15 17:49:35 -0500

    awk: don't use change_make_var

--- awk.rb
+++ awk.rb
@@ -15,10 +15,7 @@ class Awk < Formula
     # /usr/bin/bison: missing operand after `awkgram.y'
     # makefile believes -S to use sprintf instead of sprint, but the
     # -S flag is not supported by `bison -y`
-    inreplace "makefile" do |s|
-      s.change_make_var! 'YACC', 'yacc -d'
-    end
-    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "YACC=yacc -d"
     bin.install 'a.out' => 'awk'
     man1.install 'awk.1'
   end
