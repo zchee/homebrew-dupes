ed9604c98b0ab0650df7c63e4f1b4642c866a905
Patrick Wildt <patrick@blueri.se>
2014-03-17 22:53:38 +0100

    OpenSSH 6.6p1 OSX keychain support.
    Closes #287.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- openssh.rb
+++ openssh.rb
@@ -6,11 +6,27 @@ class Openssh < Formula
   version '6.6p1'
   sha256 '48c1f0664b4534875038004cc4f3555b8329c2a81c1df48db5c517800de203bb'
 
+  option 'with-keychain-support', 'Add native OS X Keychain and Launch Daemon support to ssh-agent'
+
+  depends_on 'autoconf' => :build if build.with? 'keychain-support'
   depends_on 'openssl'
   depends_on 'ldns' => :optional
   depends_on 'pkg-config' => :build if build.with? "ldns"
 
+  def patches
+    p = []
+    p << 'https://gist.githubusercontent.com/Bluerise/9400603/raw/28b1cabcc468ce67a41b866eec03032d814a8c18/OpenSSH+6.6p1+keychain+support' if build.include? 'with-keychain-support'
+    p
+  end
+
   def install
+    system "autoreconf -i" if build.with? 'keychain-support'
+
+    if build.include? "with-keychain-support"
+        ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
+        ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
+    end
+
     args = %W[
       --with-libedit
       --with-kerberos5
@@ -24,5 +40,22 @@ class Openssh < Formula
     system "./configure", *args
     system "make"
     system "make install"
+  end
+
+  def caveats
+    if build.include? "with-keychain-support"
+      <<-EOS.undent
+        For complete functionality, please modify:
+          /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
+
+        and change ProgramArugments from
+          /usr/bin/ssh-agent
+        to
+          #{HOMEBREW_PREFIX}/bin/ssh-agent
+
+        After that, you can start storing private key passwords in
+        your OS X Keychain.
+      EOS
+    end
   end
 end
