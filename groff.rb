358a43cce709d96eb2aea6104cf0d1154f0f5139
Alexis Hildebrandt <afh@surryhill.net>
2014-05-15 20:25:13 +0200

    groff: add with-gpresent option
    which installs groff macros for presentations.
    
    Closes #312.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- groff.rb
+++ groff.rb
@@ -14,6 +14,7 @@ class Groff < Formula
   # at that (temp) path for gs.
   option 'with-gropdf', 'Enable PDF output support'
   option 'with-grohtml', 'Enable HTML output support (implies --with-gropdf)'
+  option 'with-gpresent', 'Install macros for the presentations document format'
 
   # gs needed for both PDF and HTML output
   depends_on 'ghostscript' if (build.with? 'gropdf' or build.with? 'grohtml')
@@ -24,10 +25,25 @@ class Groff < Formula
   depends_on 'netpbm' if build.with? 'grohtml'
   depends_on 'psutils' if build.with? 'grohtml'
 
+  resource 'gpresent' do
+    url 'http://www.science.uva.nl/~bobd/useful/gpresent/gpresent-2.3.tar.gz'
+    sha1 '7d38165ad87ce418458275d0c04388dd0c651431'
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--without-x"
     system "make" # Separate steps required
     system "make install"
+
+    if build.with? 'gpresent'
+      resource('gpresent').stage do
+        (share/'groff/site-tmac').install Dir['*.tmac']
+        (share/'groff/examples').install Dir['*.rof', '*.pdf']
+        man7.install Dir['*.7']
+        man1.install Dir['*.1']
+        bin.install 'presentps'
+      end
+    end
   end
 
   def caveats
