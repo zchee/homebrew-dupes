02827d9560a46f696420aaa09a37ed6d1081fe64
Jack Nagel <jacknagel@gmail.com>
2014-05-24 09:56:37 -0500

    screen: use new patch syntax, fix deps

--- screen.rb
+++ screen.rb
@@ -1,31 +1,37 @@
-require 'formula'
+require "formula"
 
 class Screen < Formula
-  homepage 'http://www.gnu.org/software/screen'
-  url 'http://ftpmirror.gnu.org/screen/screen-4.0.3.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/screen/screen-4.0.3.tar.gz'
-  version '4.00.03'
-  sha1 '7bc6e2f0959ffaae6f52d698c26c774e7dec3545'
+  homepage "http://www.gnu.org/software/screen"
 
-  head 'git://git.savannah.gnu.org/screen.git', :branch => 'master'
+  stable do
+    url "http://ftpmirror.gnu.org/screen/screen-4.0.3.tar.gz"
+    mirror "http://ftp.gnu.org/gnu/screen/screen-4.0.3.tar.gz"
+    version "4.00.03"
+    sha1 "7bc6e2f0959ffaae6f52d698c26c774e7dec3545"
 
-  depends_on "autoconf" => :build
+    patch do
+      url "http://trac.macports.org/raw-attachment/ticket/20862/screen-4.0.3-snowleopard.patch"
+      sha1 "850aa680a5ce704ce56541eb023edd2c0285fa9a"
+    end
+  end
 
-  def patches
-    if build.head?
-      # This patch is to disable the error message
-      # "/var/run/utmp: No such file or directory" on launch
-      "https://gist.github.com/raw/4608863/75669072f227b82777df25f99ffd9657bd113847/gistfile1.diff"
-    else
-      "http://trac.macports.org/raw-attachment/ticket/20862/screen-4.0.3-snowleopard.patch"
+  head do
+    url "git://git.savannah.gnu.org/screen.git", :branch => "master"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+
+    # This patch is to disable the error message
+    # "/var/run/utmp: No such file or directory" on launch
+    patch do
+      url "https://gist.github.com/raw/4608863/75669072f227b82777df25f99ffd9657bd113847/gistfile1.diff"
+      sha1 "93d611f1f46c7bbca5f9575304913bd1c38e183b"
     end
   end
 
   def install
     if build.head?
-      Dir.chdir 'src'
-      system "autoconf"
-      system "autoheader"
+      cd "src"
+      system "./autogen.sh"
 
       # With parallel build, it fails
       # because of trying to compile files which depend osdef.h
