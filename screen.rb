b397cd1b9cb09e940282924fbd9210e06146add5
Jack Nagel <jacknagel@gmail.com>
2014-05-29 12:30:14 -0500

    screen: move patch into spec blocks

--- screen.rb
+++ screen.rb
@@ -7,21 +7,28 @@ class Screen < Formula
     url "http://ftpmirror.gnu.org/screen/screen-4.2.1.tar.gz"
     mirror "http://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz"
     sha1 "21eadf5f1d64120649f3390346253c6bc8a5103c"
+
+    # This patch is to disable the error message
+    # "/var/run/utmp: No such file or directory" on launch
+    patch :p2 do
+      url "https://gist.github.com/raw/4608863/75669072f227b82777df25f99ffd9657bd113847/gistfile1.diff"
+      sha1 "93d611f1f46c7bbca5f9575304913bd1c38e183b"
+    end
   end
 
   head do
     url "git://git.savannah.gnu.org/screen.git", :branch => "master"
+
+    # This patch is to disable the error message
+    # "/var/run/utmp: No such file or directory" on launch
+    patch :p1 do
+      url "https://gist.github.com/raw/4608863/75669072f227b82777df25f99ffd9657bd113847/gistfile1.diff"
+      sha1 "93d611f1f46c7bbca5f9575304913bd1c38e183b"
+    end
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-
-  # This patch is to disable the error message
-  # "/var/run/utmp: No such file or directory" on launch
-  patch build.head? ? :p1 : :p2 do
-    url "https://gist.github.com/raw/4608863/75669072f227b82777df25f99ffd9657bd113847/gistfile1.diff"
-    sha1 "93d611f1f46c7bbca5f9575304913bd1c38e183b"
-  end
 
   def install
     if build.head?
