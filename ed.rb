f28e4f7c46609995652474032233feca416f79d1
David Christenson <david@the-david.com>
2014-06-05 19:55:30 -0600

    ed 1.10
    Update to latest stable release, use HTTPS on homepage and mirror, change single quotes to double quotes.
    
    Closes #321.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- ed.rb
+++ ed.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class Ed < Formula
-  homepage 'http://www.gnu.org/software/ed/ed.html'
-  url 'http://ftpmirror.gnu.org/ed/ed-1.9.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/ed/ed-1.9.tar.gz'
-  sha1 'a7f01929eb9ae5fe2d3255cd99e41a2211571984'
+  homepage "https://www.gnu.org/software/ed/ed.html"
+  url "http://ftpmirror.gnu.org/ed/ed-1.10.tar.lz"
+  mirror "https://ftp.gnu.org/gnu/ed/ed-1.10.tar.lz"
+  sha1 "aba379f59b98ee4bae8f76f92b563827bd9e0e19"
 
-  option 'default-names', "Don't prepend 'g' to the binaries"
+  option "default-names", "Don't prepend 'g' to the binaries"
 
   def install
     args = ["--prefix=#{prefix}"]
-    args << "--program-prefix=g" unless build.include? 'default-names'
+    args << "--program-prefix=g" unless build.include? "default-names"
     ENV.j1
     system "./configure", *args
     system "make"
@@ -18,8 +18,8 @@ class Ed < Formula
   end
 
   test do
-    options = Tab.for_formula('ed').used_options
-    ed = options.any? {|t| t.name == 'default-names'} ? 'ed' : 'ged'
+    options = Tab.for_formula("ed").used_options
+    ed = options.any? {|t| t.name == "default-names"} ? "ed" : "ged"
     system bin/ed, "--version"
   end
 
