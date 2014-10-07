fd84c226a5e1d6e48dbc0138c264d1a0edf3a17d
David Christenson <david@the-david.com>
2014-10-06 19:29:52 -0600

    make 4.1
    Update to latest stable release, remove unrecognized --disable-debug
    option, fetch bzip2 package (551KB smaller than gzip), use HTTPS on
    homepage and mirror URLs, change single quotes to double.
    
    Closes #365.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- make.rb
+++ make.rb
@@ -1,24 +1,23 @@
-require 'formula'
+require "formula"
 
 class Make < Formula
-  homepage 'http://www.gnu.org/software/make/'
-  url 'http://ftpmirror.gnu.org/make/make-4.0.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/make/make-4.0.tar.gz'
-  sha1 'b092020919f74d56118eafac2c202f25ff3b6e59'
+  homepage "https://www.gnu.org/software/make/"
+  url "http://ftpmirror.gnu.org/make/make-4.1.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/make/make-4.1.tar.bz2"
+  sha1 "0d701882fd6fd61a9652cb8d866ad7fc7de54d58"
 
   keg_only :provided_by_osx
 
-  depends_on 'guile' => :optional
+  depends_on "guile" => :optional
 
   def install
     args = %W[
-      --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
 
-    args << '--with-guile' if build.with? 'guile'
-    system './configure', *args
-    system "make install"
+    args << "--with-guile" if build.with? "guile"
+    system "./configure", *args
+    system "make", "install"
   end
 end
