2d2f07c5a76c29c6dc5587d972bad5250c8447c3
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-10-11 03:08:57 +0100

    Heimdal: Keg-only.
    OS X seems to ship a Heimdal framework, and this one can interfere with that and seemingly cause some fatal build issues, discussed most recently [here](https://github.com/Homebrew/homebrew/issues/33074).
    
    Closes #368.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- heimdal.rb
+++ heimdal.rb
@@ -1,9 +1,11 @@
-require 'formula'
+require "formula"
 
 class Heimdal < Formula
-  homepage 'http://www.h5l.org'
-  url 'http://www.h5l.org/dist/src/heimdal-1.6rc2.tar.gz'
-  sha1 'f129ec039c7b62ea8226d907db270f1cc0bca7d3'
+  homepage "http://www.h5l.org"
+  url "http://www.h5l.org/dist/src/heimdal-1.6rc2.tar.gz"
+  sha1 "f129ec039c7b62ea8226d907db270f1cc0bca7d3"
+
+  keg_only :provided_by_osx
 
   depends_on :x11 => :recommended
 
@@ -17,6 +19,6 @@ class Heimdal < Formula
       args << "--without-x" if build.without? "x11"
 
       system "./configure", *args
-      system "make install"
+      system "make", "install"
   end
 end
