3cd3160ea8040fb2357ed0329e05d6fc86a9c3e7
Jack Nagel <jacknagel@gmail.com>
2014-10-23 22:08:26 -0500

    fetchmail: remove explicit xz dep
    It's detected from the URL.

--- fetchmail.rb
+++ fetchmail.rb
@@ -5,7 +5,6 @@ class Fetchmail < Formula
   url "https://downloads.sourceforge.net/project/fetchmail/branch_6.3/fetchmail-6.3.26.tar.xz"
   sha256 "79b4c54cdbaf02c1a9a691d9948fcb1a77a1591a813e904283a8b614b757e850"
 
-  depends_on "xz" => :build
   depends_on "openssl"
 
   def install
