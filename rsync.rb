9b62a5b60e7d9b73ec7c669a480a3f8e0d9ea5de
Matt Jones <msjwozere@gmail.com>
2014-10-24 09:54:13 +0100

    Changing samba patch URLs after repository move
    Closes #374.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- rsync.rb
+++ rsync.rb
@@ -8,17 +8,17 @@ class Rsync < Formula
   depends_on "autoconf" => :build
 
   patch do
-    url "https://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=fileflags.diff"
+    url "https://git.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=fileflags.diff"
     sha1 "71b88f5958f88f5c5664ed4ee10957a7dee10ff3"
   end
 
   patch do
-    url "https://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=crtimes.diff"
+    url "https://git.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=crtimes.diff"
     sha1 "d00a104eb18177d5106ccc057cbd7e54b2693447"
   end
 
   patch do
-    url "https://gitweb.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=hfs-compression.diff"
+    url "https://git.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=hfs-compression.diff"
     sha1 "b5698b26ae358658207ce3627c20aef605b6ddd4"
   end
 
