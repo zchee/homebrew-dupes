d251d8916652eda605d46a749d007436800efc53
Jack Nagel <jacknagel@gmail.com>
2014-10-23 22:10:30 -0500

    lsof: use version method

--- lsof.rb
+++ lsof.rb
@@ -1,15 +1,15 @@
-require 'formula'
+require "formula"
 
 class Lsof < Formula
-  homepage 'http://people.freebsd.org/~abe/'
-  url 'ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.88.tar.bz2'
-  sha1 '09db6d2cd96bc6832d9b767084b9c67cf5cf52bb'
+  homepage "http://people.freebsd.org/~abe/"
+  url "ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.88.tar.bz2"
+  sha1 "09db6d2cd96bc6832d9b767084b9c67cf5cf52bb"
 
   def install
-    system "tar xf lsof_4.88_src.tar"
-    cd "lsof_4.88_src" do
+    system "tar", "xf", "lsof_#{version}_src.tar"
+    cd "lsof_#{version}_src" do
       mv "00README", "../README"
-      system "./Configure -n darwin"
+      system "./Configure", "-n", "darwin"
       system "make"
       bin.install "lsof"
     end
