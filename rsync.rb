fac6030f1ac390968d37a3bc040a5a41e55f687f
Luke Olson <luke.olson@gmail.com>
2014-11-01 14:22:00 -0500

    rsync: use macports patches
    git.samba.org appears to have changed names recently 9b62a5b60 and it is
    down again.  Macports hosts these files (which are the same as
    http://rsync.samba.org/ftp/rsync/src/rsync-patches-3.1.1.tar.gz
    
    Closes #380.
    Closes #381.
    Closes #383.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- rsync.rb
+++ rsync.rb
@@ -8,18 +8,18 @@ class Rsync < Formula
   depends_on "autoconf" => :build
 
   patch do
-    url "https://git.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=fileflags.diff"
-    sha1 "71b88f5958f88f5c5664ed4ee10957a7dee10ff3"
+    url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-fileflags.diff"
+    sha1 "4ecd6496642800d5da7931796eb56a92ca233671"
   end
 
   patch do
-    url "https://git.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=crtimes.diff"
-    sha1 "d00a104eb18177d5106ccc057cbd7e54b2693447"
+    url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-crtimes.diff"
+    sha1 "cf07fd4bdf09744f16dfd8f605f5f849a651f641"
   end
 
   patch do
-    url "https://git.samba.org/?p=rsync-patches.git;a=blob_plain;hb=v3.1.1;f=hfs-compression.diff"
-    sha1 "b5698b26ae358658207ce3627c20aef605b6ddd4"
+    url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-hfs-compression.diff"
+    sha1 "6dd10b8e3f8067930cd8405982d20421222fa680"
   end
 
   def install
