71c803d42e6066193be9e67ddfa40ab4a1a37f65
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-08 05:46:00 +0000

    tcpdump 4.6.2
    Version bump, optional use of a newer libpcap, culled unrecognised
    configure options and mandatory OpenSSL dependency to escape this:
    
    # /usr/local/sbin/tcpdump --version
    
    tcpdump version 4.6.1
    libpcap version 1.5.3 - Apple version 47
    OpenSSL 0.9.8za 5 Jun 2014
    
    Closes #389.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- tcpdump.rb
+++ tcpdump.rb
@@ -1,19 +1,20 @@
-require 'formula'
+require "formula"
 
 class Tcpdump < Formula
-  homepage 'http://www.tcpdump.org/'
-  url 'http://www.tcpdump.org/release/tcpdump-4.6.1.tar.gz'
-  sha1 '437da017589a33d8b936adfe121d720fb8e74b38'
+  homepage "http://www.tcpdump.org/"
+  url "http://www.tcpdump.org/release/tcpdump-4.6.2.tar.gz"
+  sha1 "7256c47e572229de8c92f070514c1f6e6bb691d4"
 
-  head 'git://bpf.tcpdump.org/tcpdump'
+  head "git://bpf.tcpdump.org/tcpdump"
+
+  depends_on "homebrew/dupes/libpcap" => :optional
+  depends_on "openssl"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
+    system "./configure", "--prefix=#{prefix}",
                           "--enable-ipv6",
                           "--disable-smb",
                           "--disable-universal"
-    system "make install"
+    system "make", "install"
   end
 end
