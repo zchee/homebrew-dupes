aa7773fffa0231ca684280f81b94b6c6b7f7663d
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-11 19:19:29 +0000

    libpcap: kegging it
    Sticks libpcap in a keg as it shadows OS X system files, which can
    cause mountains of errors. Nothing seems to have a firm dependency on
    this libpcap, so no revisions anywhere else.
    
    Closes #390.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- libpcap.rb
+++ libpcap.rb
@@ -4,6 +4,9 @@ class Libpcap < Formula
   homepage "http://www.tcpdump.org/"
   url "http://www.tcpdump.org/release/libpcap-1.6.2.tar.gz"
   sha1 "7efc7d56f4959de8bb33a92de2e15d92105eac32"
+  revision 1
+
+  keg_only :provided_by_osx
 
   head "git://bpf.tcpdump.org/libpcap"
 
