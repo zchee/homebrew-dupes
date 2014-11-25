205c7d734153caee927c55b018a5cfc5446a9884
Jack Nagel <jacknagel@gmail.com>
2014-11-25 10:24:25 -0500

    grep 2.21

--- grep.rb
+++ grep.rb
@@ -2,13 +2,14 @@ require "formula"
 
 class Grep < Formula
   homepage "https://www.gnu.org/software/grep/"
-  url "http://ftpmirror.gnu.org/grep/grep-2.20.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/grep/grep-2.20.tar.xz"
-  sha256 "f0af452bc0d09464b6d089b6d56a0a3c16672e9ed9118fbe37b0b6aeaf069a65"
+  url "http://ftpmirror.gnu.org/grep/grep-2.21.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/grep/grep-2.21.tar.xz"
+  sha256 "5244a11c00dee8e7e5e714b9aaa053ac6cbfa27e104abee20d3c778e4bb0e5de"
 
   depends_on "pcre"
 
-  option "default-names", "Do not prepend 'g' to the binary"
+  option "with-default-names", "Do not prepend 'g' to the binary"
+  deprecated_option "default-names" => "with-default-names"
 
   def install
     pcre = Formula["pcre"].opt_prefix
@@ -23,25 +24,26 @@ class Grep < Formula
       --mandir=#{man}
     ]
 
-    args << "--program-prefix=g" unless build.include? "default-names"
+    args << "--program-prefix=g" if build.without? "default-names"
 
     system "./configure", *args
     system "make"
     system "make install"
   end
 
-  def caveats; <<-EOS.undent
-    The command has been installed with the prefix "g".
-    If you do not want the prefix, install using the "default-names" option.
-    EOS
-  end unless build.include? "default-names"
+  def caveats
+    if build.without? "default-names" then <<-EOS.undent
+      The command has been installed with the prefix "g".
+      If you do not want the prefix, install using the "default-names" option.
+      EOS
+    end
+  end
 
   test do
-    text_file = (testpath/"file.txt")
+    text_file = testpath/"file.txt"
     text_file.write "This line should be matched"
-    cmd = (build.include?("default-names")) ? "grep" : "ggrep"
-    grepped = `#{bin}/#{cmd} "match" #{text_file}`
+    cmd = build.with?("default-names") ? "grep" : "ggrep"
+    grepped = shell_output("#{bin}/#{cmd} 'match' #{text_file}")
     assert_match /should be matched/, grepped
-    assert_equal 0, $?.exitstatus
   end
 end
