9281663d32730d74fe651e020a56613904017282
Misty De Meo <mistydemeo@gmail.com>
2014-12-18 12:09:08 -0800

    file-formula 5.21
    Closes #404.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- file-formula.rb
+++ file-formula.rb
@@ -3,17 +3,20 @@ require 'formula'
 # "File" is a reserved class name
 class FileFormula < Formula
   homepage 'http://www.darwinsys.com/file/'
-  url 'ftp://ftp.astron.com/pub/file/file-5.18.tar.gz'
-  mirror 'http://fossies.org/unix/misc/file-5.18.tar.gz'
-  sha1 '8753e72097f2cc935608e3b1c57c4c4e31bd8d21'
+  url 'ftp://ftp.astron.com/pub/file/file-5.21.tar.gz'
+  mirror 'http://fossies.org/unix/misc/file-5.21.tar.gz'
+  sha1 '9836603b75dde99664364b0e7a8b5492461ac0fe'
 
   head 'https://github.com/glensc/file.git'
 
   keg_only :provided_by_osx
 
-  # Marked as fixed upstream, but not yet in a release
+  # Patch applied upstream, should be in 5.22
   # See http://bugs.gw.com/view.php?id=230
-  patch :DATA if MacOS.version < :lion
+  patch do
+    url "https://github.com/file/file/commit/f79e16aebe701fdb8e821c3c1f3504568d0c10f5.diff"
+    sha1 "7dcbf309bf013c11a6c5367bab8834050d762bd5"
+  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -22,18 +25,3 @@ class FileFormula < Formula
     system "make install"
   end
 end
-
-__END__
-diff --git a/src/getline.c b/src/getline.c
-index e3c41c4..74c314e 100644
---- a/src/getline.c
-+++ b/src/getline.c
-@@ -76,7 +76,7 @@ getdelim(char **buf, size_t *bufsiz, int delimiter, FILE *fp)
-  }
- }
- 
--ssize_t
-+public ssize_t
- getline(char **buf, size_t *bufsiz, FILE *fp)
- {
-  return getdelim(buf, bufsiz, '\n', fp);
