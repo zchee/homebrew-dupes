723257e64fe1eb01d072039fef75d5074d60d2dc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-30 04:28:06 +0000

    zlib: add 1.2.8 bottle.

--- zlib.rb
+++ zlib.rb
@@ -6,6 +6,14 @@ class Zlib < Formula
   mirror 'https://downloads.sourceforge.net/project/libpng/zlib/1.2.8/zlib-1.2.8.tar.gz'
   sha1 'a4d316c404ff54ca545ea71a27af7dbc29817088'
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/dupes"
+    cellar :any
+    sha1 "8111b1453b1ac71c0249f2e3abe2ce16372509af" => :yosemite
+    sha1 "4902071981b276d201681dc4321534f94d3f32c5" => :mavericks
+    sha1 "e431acef3aa6c11d9ae8a68c29e37d3bd7400492" => :mountain_lion
+  end
+
   keg_only :provided_by_osx
 
   option :universal
