de2b188787084cc55415858c3af3cbc1befdb470
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-01 04:24:04 +0000

    bzip2: add 1.0.6 bottle.

--- bzip2.rb
+++ bzip2.rb
@@ -3,6 +3,13 @@ class Bzip2 < Formula
   url "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
   sha1 "3f89f861209ce81a6bab1fd1998c0ef311712002"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/dupes"
+    sha1 "5d515fa12bc177239863569721caad7987e267dc" => :yosemite
+    sha1 "b250429608acd57ea5411dd6ae9b3d4675fbbffb" => :mavericks
+    sha1 "07a558b9a4271377490b876c1d1cb345ee4d3f18" => :mountain_lion
+  end
+
   keg_only :provided_by_osx
 
   def install
