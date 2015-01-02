1ea81b04a4e283e5409ec0c4ba4c5d2cc82f6510
Greg V <greg@unrelenting.technology>
2015-01-02 17:30:44 +0300

    Update less to 471
    Closes #410.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- less.rb
+++ less.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Less < Formula
   homepage 'http://www.greenwoodsoftware.com/less/index.html'
-  url 'http://www.greenwoodsoftware.com/less/less-458.tar.gz'
-  sha1 'd5b07180d3dad327ccc8bc66818a31577e8710a2'
+  url 'http://www.greenwoodsoftware.com/less/less-471.tar.gz'
+  sha1 '0d030b56f3f9c3ed68c6498876a27e0cac430538'
 
   depends_on 'pcre' => :optional
 
