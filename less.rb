f167898ae9b14181f5af0c82776c9469d26a1f84
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-02 15:38:32 +0000

    less: add 471 bottle.

--- less.rb
+++ less.rb
@@ -5,6 +5,13 @@ class Less < Formula
   url 'http://www.greenwoodsoftware.com/less/less-471.tar.gz'
   sha1 '0d030b56f3f9c3ed68c6498876a27e0cac430538'
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/dupes"
+    sha1 "39ee5dcbfa07bc32b84fc5b3be03cb571231beb7" => :yosemite
+    sha1 "a3589555443a08e2e9c858a72fe30d0bbf8a4968" => :mavericks
+    sha1 "76de5c62e907e6821309aeec0031ba8185b8ea93" => :mountain_lion
+  end
+
   depends_on 'pcre' => :optional
 
   def install
