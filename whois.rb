3898955552f814d9f23f565eb92704bd2c6d8c45
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-05 11:45:03 +0000

    whois: add 5.2.3 bottle.

--- whois.rb
+++ whois.rb
@@ -5,6 +5,14 @@ class Whois < Formula
   url "http://ftp.us.debian.org/debian/pool/main/w/whois/whois_5.2.3.tar.xz"
   sha1 "6e9738e5b181ed600567d2ddb3b5e872d08201f0"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/dupes"
+    cellar :any
+    sha1 "5d87f8328466ed9b9b8a59e1486c26b58d7da45d" => :yosemite
+    sha1 "8c22dda5e3bc601d26c933254b5d97b8f90db3c4" => :mavericks
+    sha1 "4134fa42e1410fad594e688079ee59f679f4f429" => :mountain_lion
+  end
+
   def install
     # autodie was not shipped with the system perl 5.8
     inreplace "make_version_h.pl", "use autodie;", "" if MacOS.version < :snow_leopard
