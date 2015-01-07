64a0dfccdb8403575be4f3facf792696e39df0bd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-07 13:29:45 +0000

    gzip: add 1.6 bottle.

--- gzip.rb
+++ gzip.rb
@@ -5,6 +5,14 @@ class Gzip < Formula
   mirror "http://mirror.clarkson.edu/gnu/gzip/gzip-1.6.tar.gz"
   sha1 "7ec6403090b3eaeb53ef017223cb8034eebc1f49"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/dupes"
+    cellar :any
+    sha1 "80aff7ae695f8f7ed1b4a7a68566cd7e0fac6fa4" => :yosemite
+    sha1 "d5d9891b8f1afea7c3d5288314f5bc3f6cb2a0af" => :mavericks
+    sha1 "6c75eee3fb519f841177abae22bb4a3e30d237c9" => :mountain_lion
+  end
+
   # Add support for --rsyncable option
   patch do
     url "http://cvs.pld-linux.org/cgi-bin/viewvc.cgi/cvs/packages/gzip/gzip-rsyncable.patch?revision=1.4&view=co"
