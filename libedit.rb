3cc5712cd957697ef607ec1ef64f1e71c08a6f82
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-28 01:46:46 +0000

    libedit: add 3.1 bottle.

--- libedit.rb
+++ libedit.rb
@@ -6,6 +6,14 @@ class Libedit < Formula
   version "3.1"
   sha1 "9c0fc40ac9336af9af0799bcdfd3537a6ad258ff"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-dupes"
+    cellar :any
+    sha1 "95b8fe87ed59a7ed75a4f2b2bb4aa11f68b300eb" => :yosemite
+    sha1 "b612eee30295d66ea08c79cc3dddec8d93ab91af" => :mavericks
+    sha1 "6031695f7acf4ef5dd367b5175c0c7bb7cd489ed" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
