fde46d5cd5478c857f8753b91eaf2d22bac54dc5
chdiza <chdiza@gmail.com>
2015-03-10 10:05:28 -0400

    gpatch 2.7.5
    A bzipped tarball is no longer available, so use xz'ed tarball.
    
    Closes #440.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gpatch.rb
+++ gpatch.rb
@@ -1,8 +1,8 @@
 class Gpatch < Formula
   homepage 'https://savannah.gnu.org/projects/patch/'
-  url 'http://ftpmirror.gnu.org/patch/patch-2.7.1.tar.bz2'
-  mirror 'https://ftp.gnu.org/gnu/patch/patch-2.7.1.tar.bz2'
-  sha1 '1ebb78c850887c2191e58b2254786ef4595a4c5b'
+  url "http://ftpmirror.gnu.org/patch/patch-2.7.5.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/patch/patch-2.7.5.tar.xz"
+  sha256 "fd95153655d6b95567e623843a0e77b81612d502ecf78a489a4aed7867caa299"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
