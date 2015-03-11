938afe324d12a0257791e3cbb9062429f21a02b1
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-11 03:54:01 +0000

    expect: add 5.45 bottle.

--- expect.rb
+++ expect.rb
@@ -5,6 +5,13 @@ class Expect < Formula
   url 'https://downloads.sourceforge.net/project/expect/Expect/5.45/expect5.45.tar.gz'
   sha1 'e634992cab35b7c6931e1f21fbb8f74d464bd496'
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-dupes"
+    sha256 "40ca4ae627a226a7b27a4117998f99463a4544031933f711b06ff8144a295f09" => :yosemite
+    sha256 "acbc838215f029f3a0afe17b8f388c4f5c5e4e31312d92d99e1dd7528f33eb0f" => :mavericks
+    sha256 "59d6fc12a4ce841bd6c6cf77493ec8ab3dd4e061c899373e5b70c39d81a819bb" => :mountain_lion
+  end
+
   option 'enable-threads', 'Build with multithreading support'
   option 'with-brewed-tk', "Use Homebrew's Tk (has optional Cocoa and threads support)"
 
