e895dc96b58b831de8a920bc3cf9cf7d6bb4cd51
Alex Dunn <dunn.alex@gmail.com>
2015-04-08 19:14:54 -0700

    diffstat 1.59
    Closes #450.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- diffstat.rb
+++ diffstat.rb
@@ -1,17 +1,34 @@
-require 'formula'
-
 class Diffstat < Formula
-  homepage 'http://invisible-island.net/diffstat/'
-  url 'ftp://invisible-island.net/diffstat/diffstat-1.58.tgz'
-  sha256 'fad5135199c3b9aea132c5d45874248f4ce0ff35f61abb8d03c3b90258713793'
+  homepage "http://invisible-island.net/diffstat/"
+  url "https://mirrors.kernel.org/debian/pool/main/d/diffstat/diffstat_1.59.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/diffstat/diffstat_1.59.orig.tar.gz"
+  sha256 "267d1441b8889cbefbb7ca7dfd4a17f6c8bc73bc114904c74ecad945a3dbf270"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/diffstat", "--version"
+    (testpath/"diff.diff").write <<-EOS.undent
+      diff --git a/diffstat.rb b/diffstat.rb
+      index 596be42..5ff14c7 100644
+      --- a/diffstat.rb
+      +++ b/diffstat.rb
+      @@ -2,9 +2,8 @@
+      -  url 'http://invisible-island.net/datafiles/release/diffstat.tar.gz'
+      -  version '1.58'
+      -  sha1 '7a67ecb996ea65480bd0b9db33d8ed458e5f2a24'
+      +  url 'ftp://invisible-island.net/diffstat/diffstat-1.58.tgz'
+      +  sha256 'fad5135199c3b9aea132c5d45874248f4ce0ff35f61abb8d03c3b90258713793'
+    EOS
+    output = `#{bin}/diffstat diff.diff`
+    diff = <<-EOS
+ diffstat.rb |    5 ++---
+ 1 file changed, 2 insertions(+), 3 deletions(-)
+    EOS
+    assert_equal diff, output
   end
 end
