acbf8a951ea7befe5ce84cce87249a4fed7a0d27
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-12 22:21:32 +0100

    openldap 2.4.40
    Closes #452.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- openldap.rb
+++ openldap.rb
@@ -1,29 +1,35 @@
-require 'formula'
-
 class Openldap < Formula
-  homepage 'http://www.openldap.org/software/'
-  url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.39.tgz'
-  sha256 '8267c87347103fef56b783b24877c0feda1063d3cb85d070e503d076584bf8a7'
+  homepage "http://www.openldap.org/software/"
+  url "ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.40.tgz"
+  mirror "http://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.4.40.tgz"
+  sha256 "d12611a5c25b6499293c2bb7b435dc2b174db73e83f5a8cb7e34f2ce5fa6dadb"
 
-  depends_on 'berkeley-db' => :optional
-  depends_on 'openssl'
+  depends_on "berkeley-db" => :optional
+  depends_on "openssl"
 
-  option 'with-memberof', 'Include memberof overlay'
-  option 'with-unique', 'Include unique overlay'
-  option 'with-sssvlv', 'Enable server side sorting and virtual list view'
+  option "with-memberof", "Include memberof overlay"
+  option "with-unique", "Include unique overlay"
+  option "with-sssvlv", "Enable server side sorting and virtual list view"
 
   def install
-    args = %W[--disable-dependency-tracking
-              --prefix=#{prefix}
-              --sysconfdir=#{etc}
-              --localstatedir=#{var}]
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+    ]
+
     args << "--enable-bdb=no" << "--enable-hdb=no" if build.without? "berkeley-db"
     args << "--enable-memberof" if build.with? "memberof"
     args << "--enable-unique" if build.with? "unique"
     args << "--enable-sssvlv=yes" if build.with? "sssvlv"
 
     system "./configure", *args
-    system "make install"
-    (var+'run').mkpath
+    system "make", "install"
+    (var+"run").mkpath
+
+    # https://github.com/Homebrew/homebrew-dupes/pull/452
+    chmod 0755, Dir[etc/"openldap/*"]
+    chmod 0755, Dir[etc/"openldap/schema/*"]
   end
 end
