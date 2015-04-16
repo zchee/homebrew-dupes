a5e8f7a9878c0dba8ec035882156ec46f2190a8b
Alex Dunn <dunn.alex@gmail.com>
2015-04-16 10:54:45 -0700

    ed 1.11
    Closes #453.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- ed.rb
+++ ed.rb
@@ -1,29 +1,34 @@
-require "formula"
-
 class Ed < Formula
   homepage "https://www.gnu.org/software/ed/ed.html"
-  url "http://ftpmirror.gnu.org/ed/ed-1.10.tar.lz"
-  mirror "https://ftp.gnu.org/gnu/ed/ed-1.10.tar.lz"
-  sha1 "aba379f59b98ee4bae8f76f92b563827bd9e0e19"
+  url "http://ftpmirror.gnu.org/ed/ed-1.11.tar.lz"
+  mirror "https://ftp.gnu.org/gnu/ed/ed-1.11.tar.lz"
+  sha256 "bd146ede5f225e20946ad94ef6bdf07939313bcc41dde5d2beedcea1a147a134"
 
-  option "default-names", "Don't prepend 'g' to the binaries"
+  deprecated_option "default-names" => "with-default-names"
+  option "with-default-names", "Don't prepend 'g' to the binaries"
 
   def install
-    args = ["--prefix=#{prefix}"]
-    args << "--program-prefix=g" unless build.include? "default-names"
     ENV.j1
+
+    args = ["--prefix=#{prefix}"]
+    args << "--program-prefix=g" if build.without? "default-names"
+
     system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system Dir["#{bin}/{g,}ed"].first, "--version"
+    (testpath/"test").write "Hello world"
+    `echo ',s/o//\nw' | #{bin}/ged -s #{testpath}/test`
+    assert_equal "Hell world", (testpath/"test").read.chomp
   end
 
-  def caveats; <<-EOS.undent
-    The command has been installed with the prefix 'g'.
-    If you do not want the prefix, install using the 'default-names' option.
-    EOS
+  def caveats
+    if build.without? "default-names" then <<-EOS.undent
+      The command has been installed with the prefix "g".
+      If you do not want the prefix, reinstall using the "with-default-names" option.
+      EOS
+    end
   end
 end
