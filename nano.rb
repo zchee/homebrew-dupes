e50825d3338a059a8adf1e2f22999dd1efc4db3b
Alex Dunn <dunn.alex@gmail.com>
2015-05-03 00:36:19 -0700

    nano 2.4.1
    Closes #458.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- nano.rb
+++ nano.rb
@@ -1,10 +1,14 @@
-require 'formula'
-
 class Nano < Formula
-  homepage 'http://www.nano-editor.org/'
-  url 'http://www.nano-editor.org/dist/v2.4/nano-2.4.0.tar.gz'
-  sha1 '55639cbac2d38febf16780b912b036f2023534d1'
-  head 'svn://svn.sv.gnu.org/nano/trunk/nano'
+  homepage "http://www.nano-editor.org/"
+  url "http://www.nano-editor.org/dist/v2.4/nano-2.4.1.tar.gz"
+  sha256 "6a0ceb5e1b9b9bea72d5d1f46488ace4782b1f198ea6ba558480a86d994f29d2"
+
+  head do
+    url "svn://svn.sv.gnu.org/nano/trunk/nano"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
+
   bottle do
     root_url "https://homebrew.bintray.com/bottles-dupes"
     cellar :any
@@ -13,14 +17,12 @@ class Nano < Formula
     sha256 "c600e37f85012c4b2369cbab556627f116e1de35b7f211baf33326ad1da5edcf" => :mountain_lion
   end
 
-  # see https://savannah.gnu.org/bugs/?44609
-  patch :DATA
-
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "homebrew/dupes/ncurses"
 
   def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -31,23 +33,10 @@ class Nano < Formula
                           "--enable-nanorc",
                           "--disable-nls",
                           "--enable-utf8"
-    system "make install"
+    system "make", "install"
   end
 
   test do
     system "#{bin}/nano", "--version"
   end
 end
-
-__END__
---- a/src/text.c	2015-03-22 23:45:12.000000000 -0400
-+++ b/src/text.c	2015-03-24 22:44:05.000000000 -0400
-@@ -2664,7 +2664,7 @@ const char *do_alt_speller(char *tempfil
-     ssize_t current_y_save = openfile->current_y;
-     ssize_t lineno_save = openfile->current->lineno;
-     struct stat spellfileinfo;
--    __time_t timestamp;
-+    time_t timestamp;
-     pid_t pid_spell;
-     char *ptr;
-     static int arglen = 3;
