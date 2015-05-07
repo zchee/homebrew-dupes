1dcf2a61e73b07e1c386a6290e42953ca3132f71
Ali Servet Donmez <asd@pittle.org>
2015-05-07 03:13:24 +0200

    Add less-471 as devel version
    Closes #459.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- less.rb
+++ less.rb
@@ -5,6 +5,11 @@ class Less < Formula
   url 'http://www.greenwoodsoftware.com/less/less-458.tar.gz'
   sha1 'd5b07180d3dad327ccc8bc66818a31577e8710a2'
 
+  devel do
+    url "http://greenwoodsoftware.com/less/less-471.tar.gz"
+    sha1 "0d030b56f3f9c3ed68c6498876a27e0cac430538"
+  end
+
   depends_on 'pcre' => :optional
 
   def install
