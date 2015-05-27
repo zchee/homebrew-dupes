2dcf12c478ecc441eb46f565958c8ca3cde9883d
Shaun Jackman <sjackman@gmail.com>
2015-05-27 12:45:58 -0700

    rsync: mark OS X specific patches.
    The three patches are specific to Mac OS X.
    
    Closes #463.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- rsync.rb
+++ rsync.rb
@@ -14,19 +14,21 @@ class Rsync < Formula
 
   depends_on "autoconf" => :build
 
-  patch do
-    url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-fileflags.diff"
-    sha1 "4ecd6496642800d5da7931796eb56a92ca233671"
-  end
+  if OS.mac?
+    patch do
+      url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-fileflags.diff"
+      sha1 "4ecd6496642800d5da7931796eb56a92ca233671"
+    end
 
-  patch do
-    url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-crtimes.diff"
-    sha1 "cf07fd4bdf09744f16dfd8f605f5f849a651f641"
-  end
+    patch do
+      url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-crtimes.diff"
+      sha1 "cf07fd4bdf09744f16dfd8f605f5f849a651f641"
+    end
 
-  patch do
-    url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-hfs-compression.diff"
-    sha1 "6dd10b8e3f8067930cd8405982d20421222fa680"
+    patch do
+      url "https://trac.macports.org/export/127713/trunk/dports/net/rsync/files/patch-hfs-compression.diff"
+      sha1 "6dd10b8e3f8067930cd8405982d20421222fa680"
+    end
   end
 
   def install
