fa4dd3a2575a0f2e7a3112c379638c29ef251ee0
Jack Nagel <jacknagel@gmail.com>
2015-06-23 19:06:38 -0400

    lapack: install man pages
    Closes #476.

--- lapack.rb
+++ lapack.rb
@@ -1,17 +1,22 @@
-require 'formula'
-
 class Lapack < Formula
-  homepage 'http://www.netlib.org/lapack/'
-  url 'http://www.netlib.org/lapack/lapack-3.5.0.tgz'
-  sha1 '5870081889bf5d15fd977993daab29cf3c5ea970'
+  homepage "http://www.netlib.org/lapack/"
+  url "http://www.netlib.org/lapack/lapack-3.5.0.tgz"
+  sha1 "5870081889bf5d15fd977993daab29cf3c5ea970"
+
+  resource "manpages" do
+    url "http://netlib.org/lapack/manpages.tgz"
+    version "3.5.0"
+    sha1 "fb5829fca324f7a2053409b370d58e60f3aa4e6e"
+  end
 
   depends_on :fortran
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   keg_only :provided_by_osx
 
   def install
-    system "cmake", ".", "-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}", "-DBUILD_SHARED_LIBS:BOOL=ON", "-DLAPACKE:BOOL=ON"
+    system "cmake", ".", "-DBUILD_SHARED_LIBS:BOOL=ON", "-DLAPACKE:BOOL=ON", *std_cmake_args
     system "make", "install"
+    man.install resource("manpages")
   end
 end
