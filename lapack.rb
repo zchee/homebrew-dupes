d2a940e7c2d0553d1024cae21cfcaa931bdf1691
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-24 00:40:27 +0100

    lapack: add 3.5.0 bottle.

--- lapack.rb
+++ lapack.rb
@@ -3,6 +3,14 @@ class Lapack < Formula
   url "http://www.netlib.org/lapack/lapack-3.5.0.tgz"
   sha1 "5870081889bf5d15fd977993daab29cf3c5ea970"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-dupes"
+    cellar :any
+    sha256 "b492305db3e74f7fde1d1798fbbb653c9caea0f7436e8bda5af2c19677909fc0" => :yosemite
+    sha256 "c099b310cbe3217266a9316cff77471c1c72ad0203def9776c851609e38ea789" => :mavericks
+    sha256 "cd6aea6fb9bc22942172bb532a0e6836320b28406341985a11fe9d1dd9ee62ff" => :mountain_lion
+  end
+
   resource "manpages" do
     url "http://netlib.org/lapack/manpages.tgz"
     version "3.5.0"
