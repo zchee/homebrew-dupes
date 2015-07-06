1e134548f6958c840ea45d4b936c1f1d630154da
Alex Dunn <dunn.alex@gmail.com>
2015-07-06 11:41:45 -0700

    nano 2.4.2
    Closes #485.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- nano.rb
+++ nano.rb
@@ -1,13 +1,8 @@
 class Nano < Formula
+  desc "A free (GNU) replacement for the Pico text editor"
   homepage "http://www.nano-editor.org/"
-  url "http://www.nano-editor.org/dist/v2.4/nano-2.4.1.tar.gz"
-  sha256 "6a0ceb5e1b9b9bea72d5d1f46488ace4782b1f198ea6ba558480a86d994f29d2"
-
-  head do
-    url "svn://svn.sv.gnu.org/nano/trunk/nano"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-  end
+  url "http://www.nano-editor.org/dist/v2.4/nano-2.4.2.tar.gz"
+  sha256 "c8cd7f18fcf5696d9df3364ee2a840e0ab7b6bdbd22abf850bbdc951db7f65b9"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-dupes"
@@ -15,6 +10,12 @@ class Nano < Formula
     sha256 "c0c8f2d22095fb8e0ee2ed7304a0c9c3218361d7ac5e89438dc824f9e8b03dd7" => :yosemite
     sha256 "1f7eeb6de531029a388ce39b0caf4a220e97d24557ed2a9dd3b990714ad8ee72" => :mavericks
     sha256 "7ff1b6fd739a14179e3bd7ce85527f333cbbee9a25615ee7185bcc7f8da363e0" => :mountain_lion
+  end
+
+  head do
+    url "svn://svn.sv.gnu.org/nano/trunk/nano"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
   end
 
   depends_on "pkg-config" => :build
