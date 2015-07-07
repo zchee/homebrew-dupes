529287dc4d7b85da7609085f220ae8ee04639459
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-07 02:14:20 +0100

    openssh: remove keychain option
    Just to be explicit, this option is not coming back. If you want it, you will
    need to compile your own version of OpenSSH. PRs that re-add the patch will
    be rejected.
    
    See https://github.com/Homebrew/homebrew-dupes/pull/482#issuecomment-118994372
    
    Closes #487.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- openssh.rb
+++ openssh.rb
@@ -1,6 +1,5 @@
-require "formula"
-
 class Openssh < Formula
+  desc "OpenBSD freely-licensed SSH connectivity tools"
   homepage "http://www.openssh.com/"
   url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz"
   version "6.9p1"
@@ -13,21 +12,14 @@ class Openssh < Formula
     sha256 "0112148ed980edb2288e4ef4a07952c7203d452d3b4858d13f11f2815c115e84" => :mountain_lion
   end
 
-  option "with-keychain-support", "Add native OS X Keychain and Launch Daemon support to ssh-agent"
+  # Please don't resubmit the keychain patch option. It will never be accepted.
+  # https://github.com/Homebrew/homebrew-dupes/pull/482#issuecomment-118994372
   option "with-libressl", "Build with LibreSSL instead of OpenSSL"
 
-  depends_on "autoconf" => :build if build.with? "keychain-support"
   depends_on "openssl" => :recommended
   depends_on "libressl" => :optional
   depends_on "ldns" => :optional
   depends_on "pkg-config" => :build if build.with? "ldns"
-
-  if build.with? "keychain-support"
-    patch do
-      url "https://trac.macports.org/export/135165/trunk/dports/net/openssh/files/0002-Apple-keychain-integration-other-changes.patch"
-      sha256 "bcc9b9103fe2333ec6053fcdf5aac51ca2f07138cd05b66c37c01c92585ed778"
-    end
-  end
 
   patch do
     url "https://gist.githubusercontent.com/jacknagel/e4d68a979dca7f968bdb/raw/f07f00f9d5e4eafcba42cc0be44a47b6e1a8dd2a/sandbox.diff"
@@ -36,18 +28,11 @@ class Openssh < Formula
 
   # Patch for SSH tunnelling issues caused by launchd changes on Yosemite
   patch do
-    url "https://trac.macports.org/export/135165/trunk/dports/net/openssh/files/launchd.patch"
-    sha256 "02e76c153d2d51bb0b4b0e51dd7b302469bd24deac487f7cca4ee536928bceef"
+    url "https://trac.macports.org/export/138238/trunk/dports/net/openssh/files/launchd.patch"
+    sha256 "012ee24bf0265dedd5bfd2745cf8262c3240a6d70edcd555e5b35f99ed070590"
   end
 
   def install
-    system "autoreconf -i" if build.with? "keychain-support"
-
-    if build.with? "keychain-support"
-      ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
-      ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
-    end
-
     ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
 
     args = %W[
@@ -69,39 +54,5 @@ class Openssh < Formula
     system "./configure", *args
     system "make"
     system "make", "install"
-  end
-
-  def caveats
-    if build.with? "keychain-support" then <<-EOS.undent
-        NOTE: replacing system daemons is unsupported. Proceed at your own risk.
-
-        For complete functionality, please modify:
-          /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
-
-        and change ProgramArguments from
-          /usr/bin/ssh-agent
-        to
-          #{HOMEBREW_PREFIX}/bin/ssh-agent
-
-        You will need to restart or issue the following commands
-        for the changes to take effect:
-
-          launchctl unload /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
-          launchctl load /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
-
-        Finally, add  these lines somewhere to your ~/.bash_profile:
-          eval $(ssh-agent)
-
-          function cleanup {
-            echo "Killing SSH-Agent"
-            kill -9 $SSH_AGENT_PID
-          }
-
-          trap cleanup EXIT
-
-        After that, you can start storing private key passwords in
-        your OS X Keychain.
-      EOS
-    end
   end
 end
