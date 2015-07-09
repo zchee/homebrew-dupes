1050c7ec9d0502dcddfbf344e6accb967d4fbc43
Viktor Szakats <vszakats@users.noreply.github.com>
2015-07-09 17:38:01 +0200

    openssh: add https mirror
    Closes #489.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- openssh.rb
+++ openssh.rb
@@ -2,6 +2,7 @@ class Openssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools"
   homepage "http://www.openssh.com/"
   url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz"
+  mirror "https://fossies.org/linux/misc/openssh-6.9p1.tar.gz"
   version "6.9p1"
   sha256 "6e074df538f357d440be6cf93dc581a21f22d39e236f217fcd8eacbb6c896cfe"
 
