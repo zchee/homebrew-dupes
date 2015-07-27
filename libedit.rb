48974c422c8f686764192c7fe3bf7a0d1629c61e
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-27 19:17:12 +0100

    libedit 20150325-3.1

--- libedit.rb
+++ libedit.rb
@@ -1,8 +1,9 @@
 class Libedit < Formula
+  desc "BSD-style licensed readline alternative"
   homepage "http://thrysoee.dk/editline/"
-  url "http://thrysoee.dk/editline/libedit-20140620-3.1.tar.gz"
-  version "3.1"
-  sha1 "9c0fc40ac9336af9af0799bcdfd3537a6ad258ff"
+  url "http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz"
+  version "20150325-3.1"
+  sha256 "c88a5e4af83c5f40dda8455886ac98923a9c33125699742603a88a0253fcc8c5"
 
   bottle do
     cellar :any
@@ -11,9 +12,10 @@ class Libedit < Formula
     sha1 "6031695f7acf4ef5dd367b5175c0c7bb7cd489ed" => :mountain_lion
   end
 
+  keg_only :provided_by_osx
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--enable-widec",
                           "--prefix=#{prefix}"
