df068c2c94c1ae7c07a43c120e49e48fa54628d8
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-27 19:22:03 +0100

    bzip2: rebottle

--- bzip2.rb
+++ bzip2.rb
@@ -1,7 +1,8 @@
 class Bzip2 < Formula
+  desc "Freely available high-quality data compressor"
   homepage "http://www.bzip.org/"
   url "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
-  sha1 "3f89f861209ce81a6bab1fd1998c0ef311712002"
+  sha256 "a2848f34fcd5d6cf47def00461fcb528a0484d8edef8208d6d2e2909dc61d9cd"
   revision 1
 
   bottle do
@@ -13,8 +14,9 @@ class Bzip2 < Formula
   keg_only :provided_by_osx
 
   def install
+    inreplace "Makefile", "$(PREFIX)/man", "$(PREFIX)/share/man"
+
     system "make", "install", "PREFIX=#{prefix}"
-    share.install "#{prefix}/man"
   end
 
   test do
