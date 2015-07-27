e385a46e5c6992cc1c88372867a177c536be99a4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-27 21:12:44 +0100

    libpcap: add 1.7.4 bottle.

--- libpcap.rb
+++ libpcap.rb
@@ -4,6 +4,13 @@ class Libpcap < Formula
   url "http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz"
   sha256 "7ad3112187e88328b85e46dce7a9b949632af18ee74d97ffc3f2b41fe7f448b0"
 
+  bottle do
+    cellar :any
+    sha256 "3126a1acd16a59a320d61d0fc050d05a9d0c39896ac63552be73af76cf0a1546" => :yosemite
+    sha256 "c3fa203c6bcc117e55cf611b1ce228fad2d722d26ebff34428db0f293dc6e840" => :mavericks
+    sha256 "8c2c6c198d3526df70df0be64fc9c58df44334fde1ac75dd081068027308b951" => :mountain_lion
+  end
+
   keg_only :provided_by_osx
 
   head "git://bpf.tcpdump.org/libpcap"
